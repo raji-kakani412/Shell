c8b7aead0fc5b9d2d882f8778b0e7774007515b7
rajikakani412 <raji.kakani@gmail.com>
2024-09-10 19:09:18 +0530

    updated

--- 05-Readvariables.sh
+++ 05-Readvariables.sh
@@ -4,7 +4,7 @@ echo "Please enter your username::"
 read -s USERNAME # takes input into USERNAME variable and -s will not display name in terminal for eg password we use it/sensitive 
 echo "Username entered is: $USERNAME"
 
-echo "Please enter your passowrd"
+echo "Please enter your Password"
 read -s PASSWORD
 echo "PASSWORD entered is: $PASSWORD" #this one is not used generally because it will display password in terminal
 
