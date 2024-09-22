0b94dbdb450f3ca79831c8f31b62aee684d4e4fe
rajikakani412 <raji.kakani@gmail.com>
2024-09-22 15:47:01 +0530

    updated

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/logs/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # where $0 gives us script name 17-redirectors.sh
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE= "$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER #Creates a folder shell-script
 
 USERID=$(id -u)
